44198727403c0f308a78f6bda4e62e49884c97c6
Abel Putovits <abelputovits@debian-putovits>
2018-11-10 17:01:27 +0100

    modify getEvents, send less data

--- middleware/events/getEvents.js
+++ middleware/events/getEvents.js
@@ -1,27 +1,27 @@
 module.exports = objectRepository => async (req, res, next) => {
     try {
         const query = {
-            include: [
-                {
-                    model: objectRepository.models.user,
-                    attributes: {
-                        exclude: ['password']
-                    },
-                    as: 'creator'
-                },
-                { model: objectRepository.models.category },
-                { model: objectRepository.models.sport },
-                {
-                    model: objectRepository.models.team,
-                    through: { model: objectRepository.models.attendance, attributes: [] },
-                    as: 'attendances'
-                },
-                {
-                    model: objectRepository.models.team,
-                    through: { model: objectRepository.models.ranking, attributes: [] },
-                    as: 'rankings'
-                }
-            ]
+            // include: [
+            //     {
+            //         model: objectRepository.models.user,
+            //         attributes: {
+            //             exclude: ['password']
+            //         },
+            //         as: 'creator'
+            //     },
+            //     { model: objectRepository.models.category },
+            //     { model: objectRepository.models.sport },
+            //     {
+            //         model: objectRepository.models.team,
+            //         through: { model: objectRepository.models.attendance, attributes: [] },
+            //         as: 'attendances'
+            //     },
+            //     {
+            //         model: objectRepository.models.team,
+            //         through: { model: objectRepository.models.ranking, attributes: [] },
+            //         as: 'rankings'
+            //     }
+            // ]
         };
 
         if (req.query.sportId) {
