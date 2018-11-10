57b4a62ac78e7004d4b71b9aea62fd9eae0d0f26
Abel Putovits <abelputovits@debian-putovits>
2018-11-10 17:01:49 +0100

    add getEvent endpoint

---
+++ middleware/events/getEvent.js
@@ -0,0 +1,47 @@
+module.exports = objectRepository => async (req, res, next) => {
+    try {
+        const query = {
+            include: [
+                {
+                    model: objectRepository.models.user,
+                    attributes: {
+                        exclude: ['password']
+                    },
+                    as: 'creator'
+                },
+                { model: objectRepository.models.category },
+                { model: objectRepository.models.sport },
+                {
+                    model: objectRepository.models.team,
+                    through: { model: objectRepository.models.attendance, attributes: [] },
+                    as: 'attendances'
+                },
+                {
+                    model: objectRepository.models.team,
+                    through: { model: objectRepository.models.ranking, attributes: [] },
+                    as: 'rankings'
+                }
+            ]
+        };
+
+        query.where = {
+            id: req.params.eventId
+        };
+
+        if (req.query.sportId) {
+            query.where.sportId = req.query.sportId;
+        }
+
+        const event = await objectRepository.models.event.findOne(query);
+        if (!event) {
+            return next(new Error('no such event!'));
+        }
+
+        res.locals.event = event;
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+
+    return next();
+};
