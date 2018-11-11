9c3ec90e10a40c696f54fb9f2c84965cfda2fd0b
Abel Putovits <abelputovits@debian-putovits>
2018-11-11 19:22:17 +0100

    send sportname and categoryname when getting event list

--- middleware/events/getEvents.js
+++ middleware/events/getEvents.js
@@ -1,27 +1,10 @@
 module.exports = objectRepository => async (req, res, next) => {
     try {
         const query = {
-            // include: [
-            //     {
-            //         model: objectRepository.models.user,
-            //         attributes: {
-            //             exclude: ['password']
-            //         },
-            //         as: 'creator'
-            //     },
-            //     { model: objectRepository.models.category },
-            //     { model: objectRepository.models.sport },
-            //     {
-            //         model: objectRepository.models.team,
-            //         through: { model: objectRepository.models.attendance, attributes: [] },
-            //         as: 'attendances'
-            //     },
-            //     {
-            //         model: objectRepository.models.team,
-            //         through: { model: objectRepository.models.ranking, attributes: [] },
-            //         as: 'rankings'
-            //     }
-            // ]
+            include: [
+                { model: objectRepository.models.category },
+                { model: objectRepository.models.sport }
+            ]
         };
 
         if (req.query.sportId) {
